3dd7adac27c3ae2ba5d21d598546c7461b960265
ala94709 <alaaddin.ertugrul.sagun@fokus.fraunhofer.de>
2023-10-18 12:38:38 +0200

    add requirements.txt

--- mein_hrm/settings.py
+++ mein_hrm/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # The absolute path to the directory where collectstatic will collect static files for deployment.
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 

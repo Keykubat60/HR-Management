ab58887595d60fd40c172f5ac9144652591b5f0b
alaaddin <alaaddinsagun60@gmail.com>
2023-10-19 22:40:38 +0200

    fixed Postgresql settings

--- mein_hrm/settings.py
+++ mein_hrm/settings.py
@@ -85,10 +85,15 @@ WSGI_APPLICATION = 'mein_hrm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydatabase',
+        'USER': 'myuser',
+        'PASSWORD': 'mypassword',
+        'HOST': 'db',  # Der Name des Service im docker-compose.yml
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
